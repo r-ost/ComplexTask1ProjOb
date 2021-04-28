cc539c4395d7d02b7a6bce9f95eacb81a56ee9c4
r-ost <szablanowsj@gmail.com>
2021-04-28 15:01:38 +0200

    Add Iterator for SimpleDatabase.
    Adds class to iterate through SimpleDatabase.

---
+++ task3-pol/Iterators/IDatabaseIterator.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3.Iterators
+{
+    public interface IDatabaseIterator
+    {
+        bool Next();
+        VirusData Current { get;}
+    }
+}
---
+++ task3-pol/Iterators/SimpleDatabaseIterator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Task3.Iterators
+{
+    public class SimpleDatabaseIterator : IDatabaseIterator
+    {
+        private IEnumerator<SimpleDatabaseRow> _enumerator;
+        private SimpleGenomeDatabase _genomeDatabase;
+        private VirusData _current;
+
+        public VirusData Current { get => _current; }
+
+        public SimpleDatabaseIterator(SimpleDatabase simpleDatabase, SimpleGenomeDatabase genomeDatabase)
+        {
+            _enumerator = simpleDatabase.Rows.GetEnumerator();
+            _genomeDatabase = genomeDatabase;
+            _current = null;
+        }
+
+
+        public bool Next()
+        {
+            if (_enumerator.MoveNext() == false)
+                return false;
+
+            SimpleDatabaseRow currentRow = _enumerator.Current;
+            
+            var genomes = _genomeDatabase.genomeDatas
+                      .Where(g => g.Genome == currentRow.GenomeId.ToString())
+                      .ToList();
+
+            VirusData currentRowVirusData = new VirusData
+            (
+                currentRow.VirusName,
+                currentRow.DeathRate,
+                currentRow.InfectionRate,
+                genomes
+            );
+
+            _current = currentRowVirusData;
+
+            return true;
+        }
+    }
+}
